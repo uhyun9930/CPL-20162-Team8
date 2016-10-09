5dffeed54511b9e107078c724efa64f0444477d1
junghun <hebo1221@gmail.com>
2016-10-09 16:59:04 +0900

    코드 수정

--- piManager/piManager/pinInformation.h
+++ piManager/piManager/pinInformation.h
@@ -5,13 +5,7 @@
 #include <wiringPi.h>
 
 /* Declare Raspberry Pi GPIO Pin number */
-/* For Motor */
-#define DIRECTION_MOT1		 7
-#define DIRECTION_MOT2		 3
-#define STEP_MOT1			 0
-#define STEP_MOT2			22
-#define ENABLE_MOT1			 2
-#define ENABLE_MOT2			 26
+
 /* For Temperature */
 //#define SDA					24
 //#define SCK					25
@@ -21,10 +15,5 @@
 /* For Door */
 #define INPUT_PIN			 1
 #define OUTPUT_PIN			 4
-
-/* For Power Relay */
-#define PWR_RELAY			26
-#define PWR_RELAY2			27
-#define PWR_RELAY3			23
 
 #endif
