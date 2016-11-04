2be87322a320f506d28859bfcfefebde70a40837
WOO-HYUN\우현 <uhyun9930@naver.com>
2016-11-05 02:07:32 +0900

    웹 어플리케이션

---
+++ web_uhyun/login_check.php
@@ -0,0 +1,49 @@
+﻿
+<?php
+
+
+error_reporting(0); 
+
+include("dbconnect.php");
+ 
+ // session의 시작
+ session_start();
+
+ $email = $_POST[Email];
+ $pass = $_POST[password];
+ 
+ $query  = "select *from member where email='$email' and password='$pass'";
+ $result = mysqli_query($conn,$query);
+ 
+ 
+ $count=mysqli_num_rows($result); //몇개의 데이터가 반환되었는지 확인, 1이라면 유저존재
+	 
+ if($count==1)
+ {
+  echo "
+   echo "<meta http-equiv='refresh' content = '0; url=http://localhost/Welcome.php'>";
+  ";
+  exit;
+ }
+ else 
+ {
+	 echo "
+	  <script>
+	   window.alert('아이디 혹은 비밀번호를 확인하세요.');
+	   history.back(1);
+	  </script>
+	  ";
+	  exit;
+ }
+ 
+ 	 
+ // 연결 확인
+ 
+// setcookie("hwi",$email,0,"/");
+// $hwi = $_COOKIE['hwi'];
+ 
+ mysqli_close($conn); 
+ 
+
+ //echo ("<meta http-equiv = 'Refresh' content='1; URL=green.php' >");
+?>
