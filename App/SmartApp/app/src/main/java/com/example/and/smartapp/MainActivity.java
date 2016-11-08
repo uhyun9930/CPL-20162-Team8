b46e1fde59da32bf9fd8587705aa6c7552e6fae7
house1998 <house1998@naver.com>
2016-11-08 18:52:47 +0900

    앱 초안

---
+++ App/SmartApp/app/src/main/java/com/example/and/smartapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.and.smartapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,Login.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+    }
+}
