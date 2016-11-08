5a10d567daeeb79ba9004c207c52c3fe9e7e52bb
house1998 <house1998@naver.com>
2016-11-08 21:24:15 +0900

    앱 프레임

--- App/Smart/app/src/main/java/com/example/and/smartapp/MainActivity.java
+++ App/Smart/app/src/main/java/com/example/and/smartapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.and.smartapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,Login.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
     }
 }
