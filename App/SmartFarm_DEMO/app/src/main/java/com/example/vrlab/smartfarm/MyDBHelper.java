c5de74e36db8844f57d7d638ea3aa4865abfae9b
house1998 <house1998@naver.com>
2016-11-21 09:47:14 +0900

    디비 변경

--- App/SmartFarm_DEMO/app/src/main/java/com/example/vrlab/smartfarm/MyDBHelper.java
+++ App/SmartFarm_DEMO/app/src/main/java/com/example/vrlab/smartfarm/MyDBHelper.java
@@ -18,7 +18,7 @@ public class MyDBHelper extends SQLiteOpenHelper {
         // TODO Auto-generated method stub
 
         db.execSQL("CREATE TABLE today(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + "Email TEXT"+"title TEXT, " + "date TEXT , " + "time TEXT, "
+                +"title TEXT, " + "date TEXT , " + "time TEXT, "
                 + "memo TEXT );");
 
     }
